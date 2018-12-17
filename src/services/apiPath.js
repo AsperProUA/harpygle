0adf500096e109386ecf2fbc884700361195abd2
Viktor <dictor93@gmail.com>
2018-12-17 19:43:19 +0200

    Fix merge troubles

--- src/services/apiPath.js
+++ src/services/apiPath.js
@@ -1,3 +1,5 @@
-const apiPath = 'http://ec2-18-221-180-172.us-east-2.compute.amazonaws.com:4500/';
-// const apiPath = 'http://localhost:4500/';
+//Production server:
+// const apiPath = 'http://ec2-18-221-180-172.us-east-2.compute.amazonaws.com:4500/';
+//viktor local dev server:
+const apiPath = 'http://localhost:4500/';
 export default apiPath;
